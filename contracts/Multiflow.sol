85b887a038935f27a838f2cdeb3dab2b1111e880
zach <zachanon@gmail.com>
2021-06-25 12:29:24 -0500

    basic interface outline

---
+++ contracts/Multiflow.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+contract Multiflow {
+
+    constructor() {}
+
+    function _payout() returns(bool success) {
+
+        return true;
+    }
+
+    function _verifyVotes() returns(bool) {
+
+        return true;
+    }
+
+    event VotePassed(
+    );
+}
