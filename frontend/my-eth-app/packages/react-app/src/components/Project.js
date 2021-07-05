373b157fd21560e21301adf13537cac3d8258429
HabacucMX <habacuc.vera@gmail.com>
2021-07-05 11:29:14 -0500

    Verification is now embed to project

--- frontend/my-eth-app/packages/react-app/src/components/Project.js
+++ frontend/my-eth-app/packages/react-app/src/components/Project.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Verification from './Verification';
 
 const Project = () => {
 
 return (
     <div className="container">
-    <div className="voter-side">Fund a project
+    <div className="voter-side"> 💧 Fund a project 💧
       <div class="eth">
         <input type="number" id="number" placeholder="0.0000"></input>
       </div>
@@ -12,24 +13,13 @@ return (
           Deposit
       </div>
     </div>
-    
-    <div className="project">
-      <div className="project-id">
-        Project #ID
-      </div>
-      <div className="project-description">
-      Description for the project taken from Votaro aproved proposals. Text to fill Text to fill Text to fill Text to fill Text to fill Text to fill Text to fill Text to fill Text to fill 
-      </div>
-      <div className="project-table">
-        <div className="proposal-cell1">Total payout: <div className="table-text">0.0100 eth</div></div>
-        <div className="proposal-cell2">Workers: <div className="table-text">1 of 3</div></div>
-      </div>
-    </div>
+
+    <Verification/>
     
     <div className="work-button">
           Take this work
     </div>
-    <div className="worker-side"> Complete the job</div>
+    <div className="worker-side"> ⚙️ Complete the job ⚙️</div>
   </div>
 )
 }
